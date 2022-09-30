1f70f608bb1e79e2d65c57fa83d2130fcae357ff
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-09-30 15:28:52 +0900

    Add: quadrature_by_parts_max.py

---
+++ 01/quadrature_by_parts_max.py
@@ -0,0 +1,18 @@
+# 円周率の近似値を区分求積法 (最大値) を用いて求める
+# 必要なモジュールをインポートする
+import math
+
+
+# 被積分関数f(x) = √(1-x^2)を定義する
+def f(x: float) -> float:
+    return math.sqrt(1 - x*x)
+
+
+# 分割数 (大きくするほど精度が高くなる)
+n = 1000000
+
+total = 0
+for k in range(1, n+1):
+    total += f((k-1) / n)
+
+print(4 * total / n)
