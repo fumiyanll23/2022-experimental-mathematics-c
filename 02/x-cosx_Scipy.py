ed0a320715a972359b582bf9e461efb360d0bd76
Takara4649 <ngthkuei@i.softbank.jp>
2022-10-07 18:24:12 +0900

    update:x-cosx_Scipy.py

--- 02/x-cosx_Scipy.py
+++ 02/x-cosx_Scipy.py
@@ -1,12 +1,14 @@
 # written by TAGAWA, Takara
 #x-cosxをscipyを用いてプログラム
+#scipyのインストール
 import scipy as sc
+#最適化関数のインストール
 from scipy import optimize
 def f(x):
-    return x
+    return x#f(x)=xの定義
 def g(x):
-    return sc.cos(x)
+    return sc.cos(x)#g(x)=cos(x)の定義
 def h(x):
-    return f(x)-g(x)
-T=(optimize.fsolve(h,0))
+    return f(x)-g(x)#h(x)=f(x)-g(x)の定義
+T=(optimize.fsolve(h,0))#h(x)=0を解く
 print(T)
