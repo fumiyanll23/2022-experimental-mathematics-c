0d666e6935e168ca6c0660a0a105c0ebcb3119a6
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-10-09 11:30:41 +0900

    Update: 数値計算で求めた解 v.s. 解と係数の関係から求めた解

--- 02/quadratic_equation.py
+++ 02/quadratic_equation.py
@@ -7,9 +7,22 @@ import sympy as sp
 # 変数xを定義する
 x = sp.symbols("x")
 
-# 2次方程式を解く
-anss = sp.solve(2*x**2 - 6*x + 3, x)
+# 2次方程式ax^2+bx+c = 0の各係数を設定する
+a, b, c = 2, -6, 3
 
+# 2次方程式を解く
+anss = sp.solve(a*x**2 + b*x + c, x)
+
+# 1. 2つの解を数値計算で求める
 for i in range(len(anss)):
     # メソッドevalf()を用いて解を小数で表す
     print(f"{anss[i]} = {anss[i].evalf()}")
+
+# 2. 大きい方の解を数値計算で求め，小さい方の解を解と係数の関係から求める
+large = anss[1]
+small = -(b/a) - large
+print(f"{small} = {small.evalf()}")
+print(f"{large} = {large.evalf()}")
+
+# 小さい方の解の求め方による差異を確認する
+print(f"(数値計算で求めた解) - (解と係数の関係から求めた解) = {anss[0].evalf() - small.evalf()}")
