92bfb283edf3f9d7b5e57721a0a20b8ac0da0ba6
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-10-09 11:51:34 +0900

    Update: sympy -> sympy as sp

--- 02/3x-1_frac.py
+++ 02/3x-1_frac.py
@@ -1,9 +1,9 @@
-# written by TAGAWA, Takara
+# written by TAGAWA, Takara and NARITA, Fumiya
 #3x-1=0の解を分数表示するプログラム
-import sympy
+import sympy as sp
 # 記号xを定義
-sympy.var('x')
+sp.var('x')
 # 3x -1 = 0 をxについて解く
-s = sympy.solve( 3*x- 1, x)
+s = sp.solve( 3*x- 1, x)
 #解を表示する
 print(s)
