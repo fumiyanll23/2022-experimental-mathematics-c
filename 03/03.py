6f6afb5e2dd5a250f692188aeeec0698daf85916
Takara4649 <ngthkuei@i.softbank.jp>
2022-10-17 20:27:44 +0900

    Add:03.py

---
+++ 03/03.py
@@ -0,0 +1,11 @@
+#sympyをインポート
+import sympy as sp
+#変数x,yを定義
+x = sp.Symbol('x')
+y = sp.Symbol('y')
+#それぞれの方程式の右辺を移項した式を入力
+equation1 = 2*x + 1*y - 7
+equation2 = 6*x + 3*y - 15
+#解を表示
+print(sp.solve([equation1, equation2]))
+#解なしであれば空白のリストが表示される
