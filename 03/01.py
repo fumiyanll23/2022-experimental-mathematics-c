9eaf002bee55a32e0164e8837ad10834f714e716
Takara4649 <ngthkuei@i.softbank.jp>
2022-10-17 20:24:56 +0900

    Add:update01py

--- 03/01.py
+++ 03/01.py
@@ -1,9 +1,10 @@
 #必要なモジュールをインポート#tagawatakara
-from numpy.linalg import solve
+import numpy as np
+
 #連立方程式の左辺の係数を式ごとに入力
 left = [[2, 3],
         [4, 5]]
 #連立方程式の右辺を入力
 right = [8, 14]
 #解を表示（左がx,右がy）
-print(solve(left, right))
+print(np.linalg.solve(left, right))
