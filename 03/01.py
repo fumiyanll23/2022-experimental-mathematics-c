313141f0172188bfb0e1c6c8bc252452c2a9aa31
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-11-05 17:27:12 +0900

    Update: introduced formatter for 03/

--- 03/01.py
+++ 03/01.py
@@ -1,10 +1,9 @@
-#必要なモジュールをインポート#tagawatakara
+# 必要なモジュールをインポート#tagawatakara
 import numpy as np
 
-#連立方程式の左辺の係数を式ごとに入力
-left = [[2, 3],
-        [4, 5]]
-#連立方程式の右辺を入力
+# 連立方程式の左辺の係数を式ごとに入力
+left = [[2, 3], [4, 5]]
+# 連立方程式の右辺を入力
 right = [8, 14]
-#解を表示（左がx,右がy）
+# 解を表示（左がx,右がy）
 print(np.linalg.solve(left, right))
