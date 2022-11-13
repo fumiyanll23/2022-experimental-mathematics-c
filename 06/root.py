2f21886676b0b651b7593ee46a5a7390f3c713da
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-11-14 02:37:37 +0900

    Update: a bit change  due to aliases of modules

--- 06/root.py
+++ 06/root.py
@@ -1,20 +1,20 @@
-#√1-x^2の定積分を求めるプログラム
-#必要なモジュールをインポート#Written by Takaratagawa
-import sympy as sm
-from sympy import Symbol, integrate
+# Written by Takaratagawa and NARITA, Fumiya
+# √1-x^2の定積分を求めるプログラム
+# 必要なモジュールをインポート
+import sympy as sp
 
-#変数xを定義
-x = Symbol('x')
+# 変数xを定義
+x = sp.Symbol('x')
 
-#積分の対象となる式
-expr = sm.sqrt(1-x**2)
+# 積分の対象となる式
+expr = sp.sqrt(1-x**2)
 
-#不定積分を求める
-ans1 = integrate(expr,x)
+# 不定積分を求める
+ans1 = sp.integrate(expr,x)
 
-#定積分を求める（積分範囲まで指定する）
-ans2 = integrate(expr, (x, 0, 1))
+# 定積分を求める（積分範囲まで指定する）
+ans2 = sp.integrate(expr, (x, 0, 1))
 
-#計算結果
+# 計算結果
 print("不定積分の結果：", ans1)
 print("定積分の結果：", ans2)
