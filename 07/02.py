161c9171e7e67771b7d9ef1fa354b783cd0ca5d4
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-11-21 06:59:39 +0900

    Add&Update: save each graph of solutions, except 03

--- 07/02.py
+++ 07/02.py
@@ -1,18 +1,18 @@
-#必要なモジュールをインポート
-import sympy as sym
+# written by TAGAWA, Takara and NARITA, Fumiya
+# 常微分方程式{x' + x = sin(t) (t > 0), x(0) = 0.5}を解く
+# 必要なモジュールをインポート
+import sympy as sp
 from sympy.plotting import plot
 
-sym.init_printing(use_unicode=True)
-#fを関数、xを変数として定義
-f = sym.Function('f')
-x = sym.Symbol('x')
-#一般解を求める
-eq = sym.Eq( f(x).diff(x,1) +   f(x)-sym.sin(x))
-ans = sym.dsolve(eq)
-print(ans)
-#初期条件などを入力して特殊解を求める
-ans_2 = sym.dsolve(eq, ics={f(0):0.5})
-print(ans_2)
-ans_2
-#グラフを描画
-plot(ans_2.rhs, (x, 0, 1))
+# tを変数、xを関数として定義
+t = sp.Symbol("t")
+x = sp.Function("x")
+# 一般解を求める
+eq = sp.Eq(x(t).diff(t,1) + x(t), sp.sin(t))
+ans_gen = sp.dsolve(eq)
+print(f"一般解: {ans_gen.lhs} = {ans_gen.rhs}")
+# 初期条件などを入力して特殊解を求める
+ans_sp = sp.dsolve(eq, ics={x(0): 0.5})
+print(f"特殊解: {ans_sp.lhs} = {ans_sp.rhs}")
+# グラフを描画
+plot(ans_sp.rhs, (t,0,2*sp.pi)).save("./07/img/02.png")
--- 07/05.py
+++ 07/05.py
@@ -1,19 +1,25 @@
-#必要なモジュールをインポート
-import sympy as sym
+# written by TAGAWA, Takara and NARITA, Fumiya
+# 常微分方程式{x' = -3x - 2y + 2t (t > 0), y' = 2x + y - sin(t) (t > 0), x(0) = 4.5, y(0) = -6.5}を解く
+# 必要なモジュールをインポート
+import sympy as sp
 from sympy.plotting import plot
 
-#f,gを関数、xを変数として定義
-x = sym.Symbol('x')
-f = sym.Function('f')
-g = sym.Function('g')
-#与えられた連立方程式を入力
-eq1 = sym.Eq(f(x).diff(x,1),-3*f(x)-2*g(x)+2*x)
-eq2 = sym.Eq(g(x).diff(x,1),2*f(x)+g(x)-sym.sin(x))
-#一般解を求める
-ans = sym.dsolve([eq1, eq2])
-print(ans)
-#初期条件などを入力し特殊解を求める
-ans_2 = sym.dsolve([eq1, eq2],ics={f(0):4.5, g(0):-6.5})
-print(ans_2)
-#グラフを描画f(x)が青、g(x)がオレンジ
-plot(ans_2[0].rhs,ans_2[1].rhs,(x,0,10))
+# tを変数、x,yを関数として定義
+t = sp.Symbol("t")
+x = sp.Function("x")
+y = sp.Function("y")
+# 与えられた連立方程式を入力
+eq1 = sp.Eq(x(t).diff(t,1), -3*x(t) - 2*y(t) + 2*t)
+eq2 = sp.Eq(y(t).diff(t,1), 2*x(t) + y(t) - sp.sin(t))
+# 一般解を求める
+anss_gen = sp.dsolve([eq1, eq2])
+print("一般解:")
+print(f"{anss_gen[0].lhs} = {anss_gen[0].rhs}")
+print(f"{anss_gen[1].lhs} = {anss_gen[1].rhs}")
+# 初期条件などを入力して特殊解を求める
+anss_sp = sp.dsolve([eq1, eq2], ics={x(0): 4.5, y(0): -6.5})
+print("特殊解:")
+print(f"{anss_sp[0].lhs} = {anss_sp[0].rhs}")
+print(f"{anss_sp[1].lhs} = {anss_sp[1].rhs}")
+# グラフを描画。x(t)が青、y(t)がオレンジ
+plot(anss_sp[0].rhs, anss_sp[1].rhs, (t,0,10)).save("./07/img/05.png")
