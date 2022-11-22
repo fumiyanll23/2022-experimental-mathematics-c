64ffe1442165196ac6b9a0fae1804bbe16523651
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-11-22 09:52:49 +0900

    Add&Update: solved 03

--- 07/03.py
+++ 07/03.py
@@ -8,12 +8,11 @@ from sympy.plotting import plot
 t = sp.Symbol("t")
 x = sp.Function("x")
 # 一般解を求める
-eq = sp.Eq(x(t).diff(t,1), (t**2+t+1) - (2*t+1)*x(t) - x(t)**2)
-ans_gen = sp.dsolve(eq, func=x(t))
+eq = sp.Eq(x(t).diff(t,1), (t**2+t+1) - (2*t+1)*x(t) + x(t)**2)
+ans_gen = sp.dsolve(eq)
 print(f"一般解: {ans_gen.lhs} = {ans_gen.rhs}")
 # 初期条件などを入力して特殊解を求める
 ans_sp = sp.dsolve(eq, ics={x(0): 0.5})
 print(f"特殊解: {ans_sp.lhs} = {ans_sp.rhs}")
 # グラフを描画
-plot(ans_sp.rhs, (t,0,5)).save("./07/img/03.png")
-# Warning: これは動かないプログラムです!!
+plot(ans_sp.rhs, (t,0,10)).save("./07/img/03.png")
