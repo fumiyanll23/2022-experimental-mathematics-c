e2823e6ee59f648b90e88253c5366168e763b6b7
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-11-28 16:15:15 +0900

    Update: a bit change

--- 08/01.py
+++ 08/01.py
@@ -1,4 +1,4 @@
-# written by  TAKARA ,TAGAWA
+# written by TAGAWA, Takara
 # 常微分方程式{y'' = sinx (0<x<π), y(0) = y(π) = 0}を解く
 # 必要なモジュールをインポート
 import sympy as sp
@@ -8,7 +8,7 @@ from sympy.plotting import plot
 x = sp.Symbol("x")
 y = sp.Function("y")
 # 常微分方程式を与える
-eq = sp.Eq(y(x).diff(x,2), sp.sin(x))
+eq = sp.Eq(y(x).diff(x, 2), sp.sin(x))
 # 一般解を求める
 ans_gen = sp.dsolve(eq)
 print(f"一般解: {ans_gen.lhs} = {ans_gen.rhs}")
@@ -16,4 +16,4 @@ print(f"一般解: {ans_gen.lhs} = {ans_gen.rhs}")
 ans_sp = sp.dsolve(eq, ics={y(0): 0, y(sp.pi): 0})
 print(f"特殊解: {ans_sp.lhs} = {ans_sp.rhs}")
 # 特殊解のグラフを描画する
-plot(ans_sp.rhs, (x,0,sp.pi)).save("./08/img/01.png")
+plot(ans_sp.rhs, (x, 0, sp.pi)).save("./08/img/01.png")
