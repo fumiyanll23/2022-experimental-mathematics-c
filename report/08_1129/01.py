881c6ee4350981de05aeb6ae3123534decdc7a29
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-12-20 00:20:48 +0900

    update: functionized

--- report/08_1129/01.py
+++ report/08_1129/01.py
@@ -1,35 +1,59 @@
-# written by NARITA, Fumiya
 # 1. 次の微分方程式の初期値問題の, 計算による厳密解またはプログラムによる近似解を求めよ．
-# 必要なモジュールをインポートする
+import numpy as np
 import sympy as sp
+from nptyping import Float, NDArray, Shape
 
-# 準備
 t = sp.Symbol("t")
 x = sp.Function("x")
 
+def solve_diff_eq(
+    coeffs: NDArray[Shape["4"], Float],
+    condss: NDArray[Shape["2, 2"], Float],
+    ) -> sp.Equality:
+    """微分方程式の初期値問題を解く
+
+    Args:
+        coeffs (NDArray[Shape[&quot;4&quot;], Float]): 係数
+        condss (list[list[float]]): 初期条件
+
+    Returns:
+        sp.Equality: 特解
+    """
+    vars = np.array([sp.Derivative(x(t), t, 2), sp.Derivative(x(t), t, 1), x(t), 1])
+    eq = sp.Eq(np.dot(coeffs, vars), 0)
+    ics = {}
+    for i, (k, v) in enumerate(condss):
+        if i == 0:
+            ics[x(k)] = v
+        else:
+            ics[sp.Derivative(x(t), t, 1).subs(t, k)] = v
+
+    return sp.dsolve(eq, ics=ics)
+
+
 # (1) {x''-x = 0, x(0) = -1, x'(0) = 2}
-eq = sp.Eq(sp.Derivative(x(t), t, 2) - x(t), 0)
-ics = {
-    x(0): -1,
-    sp.Derivative(x(t), t, 1).subs(t, 0): 2,
-}
-ans = sp.dsolve(eq, ics=ics)
+coeffs = np.array([1, 0, -1, 0])
+condss = np.array([
+    [0,-1],
+    [0, 2],
+])
+ans =solve_diff_eq(coeffs, condss)
 print(f"(1) {ans.lhs} = {ans.rhs}")
 
 # (2) {x''+3x'+2x = 0, x(0) = 1, x'(0) = 0}
-eq = sp.Eq(sp.Derivative(x(t), t, 2) + 3 * sp.Derivative(x(t), t, 1) + 2 * x(t), 0)
-ics = {
-    x(0): 1,
-    sp.Derivative(x(t), t, 1).subs(t, 0): 0,
-}
-ans = sp.dsolve(eq, ics=ics)
+coeffs = np.array([1, 3, 2, 0])
+condss = np.array([
+    [0,1],
+    [0,0],
+])
+ans = solve_diff_eq(coeffs, condss)
 print(f"(2) {ans.lhs} = {ans.rhs}")
 
 # (3) {x''+2x'-3x = 0, x(0) = 0, x'(0) = 2}
-eq = sp.Eq(sp.Derivative(x(t), t, 2) + 2 * sp.Derivative(x(t), t, 1) - 3 * x(t), 0)
-ics = {
-    x(0): 0,
-    sp.Derivative(x(t), t, 1).subs(t, 0): 2,
-}
-ans = sp.dsolve(eq, ics=ics)
+coeffs = np.array([1, 2, -3, 0])
+condss = np.array([
+    [0,0],
+    [0,2],
+])
+ans = solve_diff_eq(coeffs, condss)
 print(f"(3) {ans.lhs} = {ans.rhs}")
--- report/08_1129/02.py
+++ report/08_1129/02.py
@@ -1,45 +1,78 @@
-# written by NARITA, Fumiya
 # 2. 次の連立微分方程式の初期値問題の, 計算による厳密解またはプログラムによる近似解を求めよ．
-# 必要なモジュールをインポートする
+import numpy as np
 import sympy as sp
+from nptyping import Float, NDArray, Shape
 
-# 準備
 t = sp.Symbol("t")
 x = sp.Function("x")
 y = sp.Function("y")
 
+def solve_diff_eq(
+    coeffss: NDArray[Shape["2, 2"], Float],
+    condss: NDArray[Shape["2, 2"], Float],
+    )-> sp.Equality:
+    """連立微分方程式を解く
+
+    Args:
+        coeffss (NDArray[Shape[&quot;2, 2&quot;], Float]): 係数
+        condss (NDArray[Shape[&quot;2, 2&quot;], Float]): 初期条件
+
+    Returns:
+        sp.Equality: 特解
+    """
+    vars = np.array([x(t), y(t)])
+    eqs = [
+        sp.Eq(sp.Derivative(x(t), t, 1), np.dot(coeffss[0], vars)),
+        sp.Eq(sp.Derivative(y(t), t, 1), np.dot(coeffss[1], vars)),
+    ]
+    ics = {}
+    for i, (k, v) in enumerate(condss):
+        if i == 0:
+            ics[x(k)] = v
+        else:
+            ics[y(k)] = v
+
+    return sp.dsolve(eqs, ics=ics)
+
+
 # (1) {x' = y, y' = x, x(0) = -1, y(0) = 2}
-eq01 = sp.Eq(sp.Derivative(x(t), t, 1), y(t))
-eq02 = sp.Eq(sp.Derivative(y(t), t, 1), x(t))
-ics = {
-    x(0): -1,
-    y(0): 2,
-}
-anss = sp.dsolve([eq01, eq02], ics=ics)
+coeffss = np.array([
+    [0, 1],
+    [1, 0],
+])
+condss = np.array([
+    [0, -1],
+    [0, 2],
+])
+anss = solve_diff_eq(coeffss, condss)
 print("(1)")
 for ans in anss:
     print(f"{ans.lhs} = {ans.rhs}")
 
 # (2) {x' = -2x+y, y' = -y, x(0) = 1, y(0) = 2}
-eq01 = sp.Eq(sp.Derivative(x(t), t, 1), -2 * x(t) + y(t))
-eq02 = sp.Eq(sp.Derivative(y(t), t, 1), -y(t))
-ics = {
-    x(0): 1,
-    y(0): 2,
-}
-anss = sp.dsolve([eq01, eq02], ics=ics)
+coeffss = np.array([
+    [-2, 1],
+    [0, -1],
+])
+condss = np.array([
+    [0, 1],
+    [0, 2],
+])
+anss = solve_diff_eq(coeffss, condss)
 print("(2)")
 for ans in anss:
     print(f"{ans.lhs} = {ans.rhs}")
 
 # (3) {x' = -4x-5y, y' = x+2y, x(0) = 0, y(0) = -2/5}
-eq01 = sp.Eq(sp.Derivative(x(t), t, 1), -4 * x(t) - 5 * y(t))
-eq02 = sp.Eq(sp.Derivative(y(t), t, 1), x(t) + 2 * y(t))
-ics = {
-    x(0): 0,
-    y(0): -2 / 5,
-}
-anss = sp.dsolve([eq01, eq02], ics=ics)
+coeffss = np.array([
+    [-4, -5],
+    [1, 2],
+])
+condss = np.array([
+    [0, 0],
+    [0, -2/5],
+])
+anss = solve_diff_eq(coeffss, condss)
 print("(3)")
 for ans in anss:
     print(f"{ans.lhs} = {ans.rhs}")
