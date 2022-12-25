5d8719d6a895969a8eb20619359620587721d7e5
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-12-26 02:14:44 +0900

    update: 固有値を降順で返すようにした

--- report/04_1025/02.py
+++ report/04_1025/02.py
@@ -19,37 +19,44 @@ def get_eigen_pairs(mat) -> list[tuple[float, list[float]]]:
     for i, val in enumerate(vals):
         normarized_vec = vecs[:, i] / np.min(np.abs(vecs[:, i][vecs[:, i] != 0]))
         eigen_pairs.append((val, normarized_vec))
+    eigen_pairs = list(reversed(sorted(eigen_pairs, key=lambda xs: xs[0])))
 
     return eigen_pairs
 
 
 # (1) [[2,-2], [-1,3]]
-A = np.array([
-    [2,-2],
-    [-1,3],
-])
+A = np.array(
+    [
+        [2, -2],
+        [-1, 3],
+    ]
+)
 eigen_paris = get_eigen_pairs(A)
 print("(1)")
 for val, vec in eigen_paris:
     print(f"固有値: {val}, 固有ベクトル: {vec}")
 
 # (2) [[1,1,2], [0,1,1], [1,1,3]]
-A = np.array([
-    [1,1,2],
-    [0,1,1],
-    [1,1,3],
-])
+A = np.array(
+    [
+        [1, 1, 2],
+        [0, 1, 1],
+        [1, 1, 3],
+    ]
+)
 eigen_paris = get_eigen_pairs(A)
 print("(2)")
 for val, vec in eigen_paris:
     print(f"固有値: {val}, 固有ベクトル: {vec}")
 
 # (3) [[1,1,1], [1,1,0], [1,0,0]]
-A = np.array([
-    [1,1,1],
-    [1,1,0],
-    [1,0,0],
-])
+A = np.array(
+    [
+        [1, 1, 1],
+        [1, 1, 0],
+        [1, 0, 0],
+    ]
+)
 eigen_paris = get_eigen_pairs(A)
 print("(3)")
 for val, vec in eigen_paris:
