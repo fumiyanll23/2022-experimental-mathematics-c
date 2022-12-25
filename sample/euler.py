bbe7d4a1ca790f4bf1d2f698ddf4681d2af7b8b5
fumiyanll23 <fumiyanll23.public@gmail.com>
2022-12-25 22:24:52 +0900

    chore: a bit change

--- sample/euler.py
+++ sample/euler.py
@@ -1,3 +1,4 @@
+# 常微分方程式の初期値問題をEuler法で解く
 import math
 
 
@@ -11,21 +12,19 @@ def exact(t):
     return math.exp(3 * t)
 
 
-N, M = 20, 20
+N = 20
 # tは(N+1)個の成分を持つベクトル (数列)
 t = [0] * (N + 1)
 # xは(N+1)個の成分を持つベクトル (数列)
 x = [0] * (N + 1)
-# 表示する終時刻
-m = 20
 # 時間の刻み幅
 h = 0.1
 # 初期時刻
 t[0] = 0.0
 # 初期条件
 x[0] = 1.0
-# オイラー法
-for n in range(m):
+# Euler法
+for n in range(N):
     t[n + 1] = t[n] + h
     x[n + 1] = x[n] + h * f(t[n], x[n])
     print(
