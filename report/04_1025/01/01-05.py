d5447450283f4a6aa6af9f0a8ca65ce748188a5c
fumiyanll23 <fumiyanll23.public@gmail.com>
2023-01-06 02:54:31 +0900

    refactor: 関数を引数に含めた

--- report/04_1025/01/01-05.py
+++ report/04_1025/01/01-05.py
@@ -3,6 +3,40 @@
 import math
 
 
+def newton(
+    f, fx, fy, g, gx, gy, x0: float, y0: float, eps: float = 1e-7, iter_max: int = 100
+) -> tuple[float, float] | None:
+    """Newton法
+
+    Args:
+        f: 関数
+        fx: 関数fのx偏導関数
+        fy: 関数fのy偏導関数
+        g: 関数
+        gx: 関数gのx偏導関数
+        gy: 関数gのy偏導関数
+        x0 (float): xの初期値
+        y0 (float): yの初期値
+        eps (float, optional): 許容誤差. デフォルトは1e-7.
+        iter_max (int, optional): 最大反復回数. デフォルトは100.
+
+    Returns:
+        tuple[float, float] | None: 近似解 (収束しないとき ==> None)
+    """
+    ret = None
+    for _ in range(iter_max):
+        jacobian = fx(x0, y0) * gy(x0, y0) - fy(x0, y0) * gx(x0, y0)
+        dx = (-f(x0, y0) * gy(x0, y0) + fy(x0, y0) * g(x0, y0)) / jacobian
+        dy = (f(x0, y0) * gx(x0, y0) - fx(x0, y0) * g(x0, y0)) / jacobian
+        x0 += dx
+        y0 += dy
+        if f(x0, y0) < eps and g(x0, y0) < eps:
+            ret = (x0, y0)
+            break
+
+    return ret
+
+
 def f(x: float, y: float) -> float:
     return x - math.cos(y)
 
@@ -27,37 +61,9 @@ def gy(x: float, y: float) -> float:
     return -1
 
 
-def newton(
-    x0: float, y0: float, eps: float = 1e-7, iter_max: int = 100
-) -> tuple[float, float] | None:
-    """Newton法
-
-    Args:
-        x0 (float): xの初期値
-        y0 (float): yの初期値
-        eps (float, optional): 許容誤差. デフォルトは1e-7.
-        iter_max (int, optional): 最大反復回数. デフォルトは100.
-
-    Returns:
-        tuple[float, float] | None: 近似解 (収束しないとき ==> None)
-    """
-    ret = None
-    for _ in range(iter_max):
-        jacobian = fx(x0, y0) * gy(x0, y0) - fy(x0, y0) * gx(x0, y0)
-        dx = (-f(x0, y0) * gy(x0, y0) + fy(x0, y0) * g(x0, y0)) / jacobian
-        dy = (f(x0, y0) * gx(x0, y0) - fx(x0, y0) * g(x0, y0)) / jacobian
-        x0 += dx
-        y0 += dy
-        if f(x0, y0) < eps and g(x0, y0) < eps:
-            ret = (x0, y0)
-            break
-
-    return ret
-
-
 x0 = 0
 y0 = 0
-ans = newton(x0, y0)
+ans = newton(f, fx, fy, g, gx, gy, x0, y0)
 if ans:
     print(f"x = {ans[0]}, y = {ans[1]}")
 else:
